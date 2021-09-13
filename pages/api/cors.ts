a1b04404b78557bf4896f4d6efca9de872ed27b1
Nicolas Guasca <nicolas.guasca@gmail.com>
2021-09-13 19:11:11 +0200

    Set Cors on Next

---
+++ pages/api/cors.ts
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import type { NextApiRequest, NextApiResponse } from 'next';
+import Cors from "cors";
+import initMiddleware from "../../lib/init-middleware";
+import { NextApiResponse, NextApiRequest } from "next";
+
+// Initializing the cors middleware
+const cors = initMiddleware(
+  Cors({
+    methods: ["GET", "POST", "OPTIONS"]
+  })
+);
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // Run cors
+  await cors(req, res);
+
+  // Rest of the API logic
+  res.json({ message: "Hello Everyone!" });
+}
